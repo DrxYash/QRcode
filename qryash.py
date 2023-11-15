7f64f99aca676e297689a76efb90165df9a4d77c
Devil-Yash <86905380+DrxYash@users.noreply.github.com>
2023-11-16 00:26:58 +0530

    Update qryash.py

--- qryash.py
+++ qryash.py
@@ -1,3 +1,7 @@
-import qrcode as qr
-img= qr.make(input("Enter Your Link:-"))
-img.save("QRcode_by_yash.png")
+while True:
+    import qrcode as qr
+    img= qr.make(input("Enter Your Link:-"))
+    img.save("QRcode_by_yash.png")
+    repeat = input("Do You Wann Again? ")
+    if repeat =="no" or repeat == "No":
+        break
